a22121d306c93ab74acc9ce2e202e600f8754870
zachary baum <zachary@>
2016-11-07 16:29:29 -0500

    search bar function is working now

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,47 +1,22 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Component.extend({
   actions: {
     searchBestBuy() {
-      var apiKey = "dDNhiwh8mUbK6pzaUfGxynag";
+      var apiKey = config.myApiKey;
 
-      var promise = new Promise(function(resolve, reject) {
-
+      var keywords = this.get('keywords').split(" ");
+      var outputString = '';
+      keywords.forEach(function(word, i){
+        if (i === 0) {
+          outputString += "search=" + word;
+        } else {
+          outputString += "&search=" + word;
+        }
       });
-
-      // var getKeywords = new Promise(function(resolve, reject) {
-      //   resolve(keywords = this.get('keywords'));
-      // });
-
-      // function splitKeywords() {
-      //   return keywords.split(" ");
-      // };
-      //
-      // function prependKeywords() {
-      //   for (var i = 0; i <= keywords.length; i++) {
-      //     if (i = 0) {
-      //       keywords[i] = "search=" + keywords[i];
-      //     } else {
-      //       keywords[i] = "&search=" + keywords[i];
-      //     }
-      //   }
-      //   return keywords;
-      // };
-      //
-      // function buildUrl() {
-      //   var url = "https://api.bestbuy.com/v1/products((";
-      //
-      //   for (let keyword of keywords) {
-      //     url += keyword;
-      //     return url;
-      //   };
-      //
-      //   url += ")&)?apiKey=" + apiKey + "&format=json";
-      //   return url;
-      // };
-      //
-      // splitKeywords().then(prependKeywords().then(buildUrl()));
-      // console.log(url);
+      var url = "https://api.bestbuy.com/v1/products((" + outputString + ")&)?apiKey=" + apiKey + "&format=json";
+      console.log(url);
     },
   }
 });
