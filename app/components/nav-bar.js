04e78510d6fd536465a7da25f68eb90a4e9e7345
zachary baum <zachary@>
2016-11-10 10:52:41 -0500

    add categories dropdown to searchbar

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,9 +2,47 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Component.extend({
-  actions: {
+  content: [], // the select options
+  prompt: null, // the select 'label', has no value. optional
+  optionValuePath: 'value',
+  optionLabelPath: 'label',
+  selectedCategory: '',
+
+  // failsafe if select list is empty
+  init: function() {
+    this._super(...arguments);
+    if (!this.get('content')) {
+      this.set('content', []);
+    }
+  },
+  
+actions: {
+    change: function() {
+      let selectedIndex = this.$('select')[0].selectedIndex;
+      let content = this.get('content');
+
+      // decrement index by 1 if we have a prompt
+      let hasPrompt = !this.get('prompt');
+      let contentIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
+      let _selection = content[contentIndex - 1];
+
+      // this.set('selectedCategory', _selection.value);
+      // this.sendAction('willChangeAction', _selection.value);
+
+      if (this.get('optionValuePath')) {
+        this.set('selection', _selection[this.get('optionValuePath')]);
+      } else {
+        this.set('selection', _selection);
+      }
+
+      // this.sendAction('didChangeAction', _selection.value);
+      this.set('selectedCategory', _selection.value);
+      console.log(this.get('selectedCategory'));
+    },
+
     searchBestBuy() {
-      var apiKey = config.myApiKey;
+      var apiKey = 'dDNhiwh8mUbK6pzaUfGxynag';
+      var selectedCategory = this.get('selectedCategory');
 
       var keywords = this.get('keywords').split(" ");
       var outputString = '';
@@ -18,11 +56,11 @@ export default Ember.Component.extend({
 
       console.log(apiKey);
       var url = {
-        request: 'https://api.bestbuy.com/v1/products((' + outputString + '))?apiKey=' + apiKey + '&sort=sku.asc&show=sku,regularPrice,thumbnailImage,upc,salePrice,name,description,image,modelNumber,onSale&facet=color&format=json'
+        request: "https://api.bestbuy.com/v1/products((" + outputString + ")&(categoryPath.id=" + selectedCategory + "))?apiKey=" + apiKey + "&sort=bestSellingRank.dsc&show=name,salePrice,regularPrice,description,bestSellingRank,onSale,upc,url,thumbnailImage,dollarSavings&pageSize=100&format=json"
       };
 
-      console.log(url);
+      console.log(url.request);
       this.sendAction('searchBestBuy', url);
-    }
+    },
   }
 });
---
+++ app/components/select-menu.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  content: [], // the options
+  prompt: null, // the 'label', has no value. optional
+  optionValuePath: 'value',
+  optionLabelPath: 'label',
+
+// failsafe if list is empty
+  init: function() {
+    this._super(...arguments);
+    if (!this.get('content')) {
+      this.set('content', []);
+    }
+  },
+
+  actions: {
+    change: function() {
+      let selectedIndex = this.$('select')[0].selectedIndex;
+      let content = this.get('content');
+
+      // decrement index by 1 if we have a prompt
+      let hasPrompt = !this.get('prompt');
+      let contentIndex = hasPrompt ? selectedIndex - 1 : selectedIndex;
+      let _selection = content[contentIndex];
+
+      this.sendAction('willChangeAction', _selection);
+
+      if (this.get('optionValuePath')) {
+        this.set('selection', _selection[this.get('optionValuePath')]);
+      } else {
+        this.set('selection', _selection);
+      }
+
+      this.sendAction('didChangeAction', _selection);
+    }
+  }
+});
