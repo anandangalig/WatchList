e0a2fe7aaa52bce7f41835bc24888b968ea5ee9d
Anand Angalig <anandangalig@gmail.com>
2017-01-05 09:57:05 -0500

    add more styling; add feedback with button press

--- app/routes/results.js
+++ app/routes/results.js
@@ -12,6 +12,9 @@ export default Ember.Route.extend({
   actions:{
     addToList(item) {
       this.get('watchList').add(item);
+      var item_id = '#' + item.upc;
+      // Ember.$(item_id).text("Added to WatchList");
+      Ember.$(item_id).replaceWith("<button class='btn btn-success'>Added to WatchList</button>");
     },
     searchBestBuy(url) {
       this.transitionTo('results', url.request);
